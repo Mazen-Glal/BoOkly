db57f8641ce7ece0f4fc2abcd3b7031bac9be76d
Mazen-Glal <mazenglal19@gmail.com>
2023-02-18 14:38:08 +0200

    add custom_button.dart

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,27 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key,this.textColor,required this.buttonStyle,required this.text,required this.onPressed});
+  final String text ;
+  final ButtonStyle buttonStyle;
+  final Function() onPressed;
+  final Color? textColor;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        style: buttonStyle,
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: Styles.rate16.copyWith(
+            fontWeight: FontWeight.bold,
+            color:textColor?? Colors.white
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/item_rate.dart';
@@ -17,13 +18,12 @@ class BookDetailsViewBody extends StatelessWidget {
           const CustomBookDetailsAppBar(),
           const SizedBox(height: 5),
           const SizedBox(
-            width: 210,
-            height: 284,
-            child: Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: FeaturedListViewItem(),
-            )
-          ),
+              width: 210,
+              height: 284,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: FeaturedListViewItem(),
+              )),
           const SizedBox(
             height: 13,
           ),
@@ -46,8 +46,44 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 41),
+          const BooksAction(),
         ],
       ),
+    );
+  }
+}
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            child: CustomButton(
+              textColor: Colors.black,
+              onPressed: () {},
+              buttonStyle:  const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xffFFFFFF)),
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.horizontal(left: Radius.circular(16))
+                  ))
+              ),
+              text: '19.99€',
+            )),
+        Expanded(
+          child: CustomButton(
+          onPressed: () {},
+          buttonStyle: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Color(0xffEF8262)),
+              shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.horizontal(right: Radius.circular(16))
+              ))
+          ),
+          text: 'Free Preview',
+        )),
+      ],
     );
   }
 }
