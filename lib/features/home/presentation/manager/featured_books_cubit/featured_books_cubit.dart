b88efe970dee9a5a0822b5bcc38f6b79669eaef5
Mazen-Glal <mazenglal19@gmail.com>
2023-02-28 15:01:08 +0200

    create fetchFeaturedBooks method is done

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,25 @@
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBooksStates>{
 
-  FeaturedBookCubit():super(FeaturedBookInitialState());
+  FeaturedBookCubit(this.homeRepo):super(FeaturedBookInitialState());
 
   static FeaturedBookCubit get(context)=> BlocProvider.of(context);
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks()async {
+    emit(FeaturedBookLoadingState());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+       (failure) {
+         emit(FeaturedBookFailureState(failure.errorMessage));
+       },
+       (books) {
+         emit(FeaturedBookSuccessState(books));
+       }
+    );
+  }
 
 }
